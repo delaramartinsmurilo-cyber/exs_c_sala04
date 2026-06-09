e18c96a94ff8aedebc04e4f50e193f7b8df5d933
Murilo <delaramartinsmurilo@gmail.com>
2026-06-09 12:25:23 -0300

    Add files via upload

---
+++ C-sala04/idade.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    int idade;
+    printf("qual sua idade");
+    scanf("%d", &idade);
+    printf("a idade informada foi: %d", idade);
+
+    system("pause");
+    return 0;
+}
